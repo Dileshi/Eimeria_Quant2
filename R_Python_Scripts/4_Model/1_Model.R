a53a598d6e18ee32d96272c65b1bc6043b9e8c52
Dileshi <dileshib@gmail.com>
2022-10-26 09:42:46 +0200

    Model further developed

--- R_Python_Scripts/4_Model/1_Model.R
+++ R_Python_Scripts/4_Model/1_Model.R
@@ -1,7 +1,7 @@
 ##Plot statistical model for primary (E64,E88) and secondary infection (E64)
 #Compares effect of being primary/secondary E64:E64 & E88:E64 for DNA, Weight, Oocyst
 
-library(dplyr)
+library(tidyverse)
 
 ########IMPORT E88 PRIMARY DATA (E88_Prim)
 if(!exists("sample.data")){
@@ -28,10 +28,15 @@ E64_Prim <- E64_Prim %>% rename(labels = 'Sample')
 E64_Prim_OPG <- read.csv("Output_Data/E64_Primary_Victor_all.csv") #OPG, Genome copy calulations + other data
 E64_Prim<- left_join(E64_Prim_OPG, E64_Prim, by="labels") ## Add qPCR data
 E64_Prim <- E64_Prim[c(1:6,9,23,55:57,60,61)]
+E64_Prim <- E64_Prim[-c(6,7)]
 rm(E64_Prim_OPG)
 E64_Prim$labels<-paste0('E57a', E64_Prim$labels)
+E64_Prim <- E64_Prim %>% rename(Tm = 'Tm_mean')
+##filter out all negative infections
+#E64_Prim <- E64_Prim %>% 
+  #dplyr::filter(!Infection == "Negative")
 
-########IMPORT E64 Secondary DATA (E64_Secon)
+########IMPORT E64:E64 Secondary DATA (E64_Secon)
 if(!exists("DNA.data")){
   source("R_Python_Scripts/2_Challange_infections/2_qPCR_Data_Prep.R")
 }
@@ -39,24 +44,40 @@ if(!exists("challenge.DNA")){
   source("R_Python_Scripts/2_Challange_infections/2_qPCR_Data_Prep.R")
 }
 E64_E64 <- challenge.DNA
+E64_E64 <- E64_E64[-c(2,8,9,11,15,16)]
+##filter out all negative infections
+#E64_E64 <- E64_E64 %>% 
+  #dplyr::filter(!Infection == "Negative")
 rm(challenge.DNA)
 
-
 ########
-##E88 secondary samples in sdt dataframe 
+##E88 secondary mice  which where E88 primary infected in sdt dataframe 
 E88_E88 <- sdt[sdt$challenge_infection == "E88", ]
 
-
-
-#E88:E88 + E88:E64
+#E88 primary and E64 secondary mice in sdt dataframe
+E88_E64 <- sdt[sdt$challenge_infection == "E64", ]
 rm(sdt)
 
+####combine E64 primary and E64 challenge into one data frame 
+##first add column primary
+E64_Prim$Variable <- paste0('Primary')
+##first add column secondary
+E64_E64$Variable <- paste0('Secondary')
+#merge E64_Primary with E64:E64 secondary
+E64_total <- rbind(E64_Prim,E64_E64)
+rm(E64_E64,E64_Prim)
 
 
+#########PLOT MODEL 
+E64_Model <- lm(log10(Genome_copies_gFaeces)~dpi+Variable, data=E64_total)
+
+ggplot(E64_total,aes(y=log10(Genome_copies_gFaeces),x=Variable))+
+  geom_point()+
+  geom_smooth(method="lm")
 
 
 #https://mspeekenbrink.github.io/sdam-r-companion/factorial-anova.html 
-#
+
 
 
 
